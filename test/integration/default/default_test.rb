9828368d5c95c162eff1279774c1bb1d1593b5d8
dean louden <loudend@fan.gov>
2019-07-16 21:59:14 +0000

    added test file

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,3 +1,4 @@
+
 # InSpec test for recipe apache::default
 
 # The InSpec reference, with examples and extensive documentation, can be
@@ -11,6 +12,17 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+describe package('apache2') do
+   it { should be_installed }
+end
+describe file('/var/www/html/index.html’) do
+  it { should exist }
+  its(‘content’) { should match(/Hello Pipeline World!/) }
+end
+describe upstart_service(‘apache2’) do
+   it { should be_enabled }
+   it { should be_running }
 end
